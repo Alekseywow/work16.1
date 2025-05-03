816b8b53be53297eb13118709abe909a07d8fd18
Aleksey Danilov <danilov20022@gmail.com>
2025-05-03 20:54:10 +0500

    added two tests

--- src/test/java/StatusTests.java
+++ src/test/java/StatusTests.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 
 public class StatusTests {
@@ -32,4 +33,27 @@ public class StatusTests {
                     .body("total", is(5));
     }
 
+    @Test
+    void checkTotalWithSomeLogs(){
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().body()
+                .body("total", is(5));
+    }
+
+
+    @Test
+    void checkTotalWithStatusLogs(){
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(5))
+                .body("browsers.chrome", hasKey("127.0"));
+    }
 }
